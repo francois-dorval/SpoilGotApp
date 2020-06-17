fe4ceb5c2368e2f83f7e0e5ceaf926520b23518c
c5819 <francois.dorval@gmail.com>
2020-06-18 01:01:15 +0200

    ko

--- src/main/java/com/fdorval/spoilgot/SpoilGotApplication.java
+++ src/main/java/com/fdorval/spoilgot/SpoilGotApplication.java
@@ -14,6 +14,9 @@ public class SpoilGotApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpoilGotApplication.class, args);
 
+
+        pouet
+
     }
 
 
---
+++ src/test/java/com/fdorval/spoilgot/unit/UnitTest.java
@@ -0,0 +1,13 @@
+package com.fdorval.spoilgot.unit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+
+    @Test
+    public void monTest(){
+        Assert.assertEquals(1+1,3 );
+    }
+}
